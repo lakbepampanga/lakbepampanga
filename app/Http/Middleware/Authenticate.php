f0bf07a079f7a376648b15ddaac9defe81265349
michael <michaelreyy28@gmail.com>
2025-02-14 16:26:28 +0800

    fixed middleware auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('home');  // Changed from route('login') to route('home')
         }
     }
 }
